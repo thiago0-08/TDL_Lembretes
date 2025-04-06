20dd073880b7e61cd021c4006816c104e0d46031
thiago0-08 <housezx210@gmail.com>
2025-04-06 13:28:09 -0300

    criacao da lista de lembrete

--- lib/pages/ListaDeTarefas.dart
+++ lib/pages/ListaDeTarefas.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(const ListTileApp());
 
-class Lista_de_TarefasPage extends StatelessWidget {
+class ListTileApp extends StatelessWidget {
+  const ListTileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const ListTileExample(),
+    );
+  }
+}
+
+class ListTileExample extends StatelessWidget {
+  const ListTileExample({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Suas Tarefas")),
-      body: Center(child: Text("Página de Lista de Tarefas")),
-     
+      appBar: AppBar(title: const Text('Lista de Tarefas ')),
+      body: ListView(
+        children: const <Widget>[
+          ListTile(
+            leading: CircleAvatar(child: Text('A')),
+            title: Text('Atividade Ciencia de Dados - 10:00 AM'),
+            subtitle: Text('Atividade para ser entregue na Quarta feira'),
+          ),
+          Divider(height: 0),
+          ListTile(
+            leading: CircleAvatar(child: Text('B')),
+            title: Text('Dentista - 13:40 '),
+            subtitle: Text('Consulta de rotina com o dentista.'),
+          ),
+          Divider(height: 0),
+          ListTile(
+            leading: CircleAvatar(child: Text('C')),
+            title: Text('Atividade de Frontend - 17:00'),
+            subtitle: Text("Atividade Formulario para o curso de Frontend."),
+
+            isThreeLine: true,
+          ),
+          Divider(height: 0),
+        ],
+      ),
     );
   }
 }
